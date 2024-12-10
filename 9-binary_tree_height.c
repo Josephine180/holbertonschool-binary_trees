413f81fad1ac901ed7ee5a4cc4cdbfe298bb76ad
Josephine180 <pierre_josephine@hotmail.com>
2024-12-10 14:45:34 +0100

    betty espace task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -13,7 +13,7 @@ int right_height;
 if (tree == NULL)
 return (0);
 if (tree->left == NULL && tree->right == NULL)
-return(0);
+return (0);
 left_height = binary_tree_height(tree->left);
 right_height = binary_tree_height(tree->right);
 if (left_height >= right_height)
