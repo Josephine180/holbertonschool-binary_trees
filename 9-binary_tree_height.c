00816eaca00dac469a37675fd2fbfc31885aeec8
Josephine180 <pierre_josephine@hotmail.com>
2024-12-10 14:43:51 +0100

    task 7 checker ok

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,6 +8,16 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+int left_height;
+int right_height;
 if (tree == NULL)
 return (0);
-binary
+if (!tree->left && !tree->right)
+return(0);
+left_height = binary_tree_height(tree->left);
+right_height = binary_tree_height(tree->right);
+if (left_height >= right_height)
+return (left_height + 1);
+else
+return (right_height + 1);
+}
