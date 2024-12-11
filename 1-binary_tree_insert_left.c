5f1926daf4a0eb5e79bbeb4f3911ebef51c81b7f
Josephine180 <pierre_josephine@hotmail.com>
2024-12-11 14:55:42 +0100

    task1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -11,26 +11,30 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
-if (new_node == NULL)
-return (NULL);
-/* on verifie l'alloction de mémoire */
+binary_tree_t *new_node;
 if (parent == NULL)
 return (NULL);
 /* si parent est null on retourne null */
+new_node = malloc(sizeof(binary_tree_t));
+if (new_node == NULL)
+return (NULL);
+/* on verifie l'alloction de mémoire */
 new_node->n = value;
 /* on assigne une valeur au nouveau noeud */
 new_node->parent = parent;
 /* on pointe le nouveau noeud vers son parent qui est parent */
 /* arbre binaire = hierarchie on doit savoir le parent */
-new_node->left = parent->left;
+new_node->left = NULL;
 /* si enfant, on le relie au nouveau noeud gauche */
 /* l'enfant devient l'enfant du nouveau noeud */
 new_node->right = NULL;
 /* initialisation du nouveau champs right à null */
 if (parent->left != NULL)
-parent->left->parent = new_node;
+{
+new_node->left = parent->left;
 /* Set the new node as the left child of the parent */
+parent->left->parent = new_node;
+}
 parent->left = new_node;
 return (new_node);
 }
