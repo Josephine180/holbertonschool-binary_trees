5a78625c135e17706c809d99e7bfe408516d9bff
Josephine180 <pierre_josephine@hotmail.com>
2024-12-11 15:20:33 +0100

    mauvais return

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -2,7 +2,7 @@
 /**
 * binary_tree_is_full - function that checks if a binary tree is full
 * @tree: pointer to the node
-* Result: 1 or 0 if tree is NULL
+* Return: 1 or 0 if tree is NULL
 */
 
 int binary_tree_is_full(const binary_tree_t *tree)
@@ -15,7 +15,9 @@ return (1);
 /* on verifie si le noeud a pas d'enfants*/
 /* s'il n'a pas d'enfants il est plein à son niveau */
 if (tree->left != NULL && tree->right != NULL)
+{
 return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
 /* si le noeud a deux enfants on verifie recursivement*/
+}
 return (0);
 }
