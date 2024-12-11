d96bc18dd60fd286bec03041f841de8c103eba99
Josephine180 <pierre_josephine@hotmail.com>
2024-12-11 15:58:23 +0100

    test task 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,52 +1,5 @@
 #include "binary_trees.h"
-/**
- * binary_tree_height - Helper function to calculate the height of the tree
- * @tree: Pointer to the root node of the tree to measure the height
- *
- * Return: The height of the tree or 0 if tree is NULL
- */
-static size_t binary_tree_height(const binary_tree_t *tree)
-{
-	size_t left_height = 0;
-	size_t right_height = 0;
-
-	if (tree == NULL)
-	return (0);
-
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
-
-	return ((left_height > right_height ? left_height : right_height) + 1);
-}
-
-/**
- * is_perfect_recursive - Helper function to check if the tree is perfect
- * @tree: Pointer to the root node of the tree to check
- * @depth: Current depth of the node
- * @level: The level of the leaf nodes
- *
- * Return: 1 if the tree is perfect otherwise 0
- */
-static int is_perfect_recursive(const binary_tree_t *tree, size_t depth, size_t level)
-{
-	if (tree == NULL)
-		return (1);
-	/*Si node est leaf*/
-	if (!tree->left && !tree->right)
-	{
-		if (level == depth)/*Verifier si leaf est au bon niveau*/
-			return (1);/*Tree est parfait a ce niveau*/
-		else
-			return (0);
-	}
-	/*Si node a un seul enfant, tree n'est pas parfait*/
-	if (!tree->left || !tree->right)
-		return (0);
-	/*Appeler recursivement la fonction pour sous arbres gauche et droit*/
-	return (is_perfect_recursive(tree->left, depth, level + 1) &&
-		is_perfect_recursive(tree->right, depth, level + 1));
-}
-
+#include "9-binary_tree_height.c"
 /**
  * binary_tree_is_perfect - Checks if a binary tree is perfect
  * @tree: A pointer to the root node of the tree to check
@@ -55,13 +8,17 @@ static int is_perfect_recursive(const binary_tree_t *tree, size_t depth, size_t 
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	size_t depth;
-
-	/*Si tree est vide, pas parfait*/
-	if (!tree)
-		return (0);
-	/*Calculer profondeur de tree (hauteur last level)*/
-	depth = binary_tree_height(tree) - 1;
-	/*Verifier si tree est  parfait en utilisant fonction recursive*/
-	return (is_perfect_recursive(tree, depth, 0));
+int height_left, height_right;
+if (tree == NULL)
+return (0);
+height_left = binary_tree_height(tree->left);
+height_right = binary_tree_height(tree->right);
+/* si sous-arbres ne sont pas égaux */
+if (height_left != height_right)
+return (0);
+/*si noeud gauche et droit sont des feuilles c'est parfait */
+if (tree->left == NULL && tree->right == NULL)
+return (1);
+return (binary_tree_is_perfect(tree->left)
+&& binary_tree_is_perfect(tree->right));
 }
